b58412ea3fc5b22eb9c98bd4a6e394afcbf6ac4a
LukaszSl <lukasz.slupik@johncube.pl>
2020-12-11 21:58:31 +0100

    Parsing for any period

--- server.js
+++ server.js
@@ -1,37 +1,79 @@
 const express = require("express")
 const port = 3000;
-const fs = require('fs')
 
 const app = express()
 
 const data = require('./data.json')
 
-const parseData = async () => {
+// "Afghanistan": {
+//     "population": 38928341,
+//         "totals": {
+//         "deaths": 253719,
+//             "recovered": 5166205,
+//                 "confirmed": 7435755
+//     },
+//     "avg": {
+//         "deaths": 0.006517590872932397,
+//             "recovered": 0.13271063876058833,
+//                 "confirmed": 0.19101135083049134
+//     },
+//     "days": [
+//     {
+//         "date": "2020-01-23",
+//         "timestamp": 1579734000,
+//         "incremental": {
+//             "deaths": 0,
+//             "recovered": 0,
+//             "confirmed": 0,
+//             "avg": {
+//                 "deaths": 0,
+//                 "recovered": 0,
+//                 "confirmed": 0
+//             }
+//         },
+//         "diff": {
+//             "deaths": 0,
+//             "recovered": 0,
+//             "confirmed": 0,
+//             "avg": {
+//                 "deaths": 0,
+//                 "recovered": 0,
+//                 "confirmed": 0
+//             }
+//         }
+//     },
+// ]
+// }
+
+const parseData = async (period) => {
     let returnData = {}
-    console.log(returnData)
     for (let i in data) {
-        // console.log(data[i].population)
+        console.log("====")
         returnData[i] = {}
         returnData[i].name = i
         returnData[i].population = data[i].population
+
         returnData[i].lastDays = []
-        for (let x = data[i].days.length - 5; x < data[i].days.length; x++) {
-            returnData[i].lastDays.push(data[i].days[x].incremental.confirmed - data[i].days[x - 1].incremental.confirmed)
+        let lastPeriod;
+        for (let x = data[i].days.length - 5 * period; x < data[i].days.length; x += period) {
+            lastPeriod = data[i].days[x].incremental.confirmed - data[i].days[x - period].incremental.confirmed
+            // console.log(lastPeriod)
+            // console.log(x)
+            returnData[i].lastDays.push(data[i].days[x].incremental.confirmed - data[i].days[x - period].incremental.confirmed)
         }
+        
         returnData[i].points = []
         for (let j = 0; j < data[i].days.length; j++) {
             returnData[i].points.push({ x: j, y: data[i].days[j].incremental.confirmed })
         }
     }
-    // for(let x in returnData) console.log(returnData[x])
-    // console.log(returnData)
 
     return returnData
 }
 
 app.get('/', async (req, res) => {
-    // console.log(data)
-    const returnData = await parseData()
+    // np. ?period=7
+    const returnData = await parseData(+req.query.period)
     res.status(200).json(returnData)
 })
 
